30da126f0585ec29728de4b6b460c25bfef900e2
giacomo-piperno <giacomopiperno@cnr.it>
2024-06-06 18:44:25 +0200

    Deleted old code

--- old/BCMGX/src/basic_kernel/halo_communication/extern.h
+++
@@ -1,9 +0,0 @@
-#ifndef PICO_EXTERN
-#define PICO_EXTERN
-
-#include "localization_debug.h"
-
-extern pico_error_info pico_info;
-extern pico_compare_cnt pico_compare;
-
-#endif
--- old/BCMGX/src/solver/bcmgx.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "basic_kernel/matrix/CSR.h"
-#include "prec_setup/matchingAggregation.h"
-
-vector<vtype>* bcmgx(CSR *Alocal, vector<vtype> *rhs, const params p, bool precondition_flag);
